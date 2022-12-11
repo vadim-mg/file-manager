cf249960b9c9243414815bcea6d3bd787917c6b2
vadim-mg <vadikm1981@yandex.ru>
2022-12-11 12:27:41 +0500

    feat: implement error_messages

---
+++ src/errors/error_messages.js
@@ -0,0 +1,14 @@
+const param = (name) => (name.length ? `"${name}"` : "")
+
+export const NOT_NEED_PARAMS = (name = "") =>
+  `command ${param(name)} doesn\'t support any parameters!`
+
+export const NEED_ONE_PARAM = (name = "", parameters = "") =>
+  `command ${param(name)} must have one parameter ${param(parameters)}`
+
+export const NO_SUCH_PATH = (targetPath = "") =>
+  `No such file or directory ${param(targetPath)}`
+
+export const NOT_PERMIT = "Operation not permitted"
+
+export const UNKNOWN = (code = "") => `Error with code:${param(code)}`
