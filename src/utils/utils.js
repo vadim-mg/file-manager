664426b689f89222a2702b20e6fed26f317b96f4
vadim-mg <vadikm1981@yandex.ru>
2022-12-11 12:24:48 +0500

    feat: implement currentDate function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,4 +11,13 @@ const caseInsensitiveSort = (a, b) => {
   return 0
 }
 
-export { caseInsensitiveSort }
+const currentDate = () => {
+  let ts = Date.now()
+  let date_ob = new Date(ts)
+  let date = date_ob.getDate()
+  let month = date_ob.getMonth() + 1
+  let year = date_ob.getFullYear()
+  return year + "-" + month + "-" + date
+}
+
+export { caseInsensitiveSort, currentDate }
