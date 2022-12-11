2e7b4734cd6a004160b7cae93c2478cbd9c74328
vadim-mg <vadikm1981@yandex.ru>
2022-12-11 12:32:25 +0500

    feat: extend Error classes, add writing to logger

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,14 +1,31 @@
-class InvalidInputError extends Error {
+import { logger } from "./logger.js"
+import { currentDate } from "../utils/utils.js"
+
+class fileManagerError extends Error {
+  constructor(message, errorForDebug = null) {
+    super(message)
+    this.errorForDebug = errorForDebug
+
+    if (errorForDebug) {
+      logger.error(`--------${currentDate()}---------`)
+      logger.error(`${this.name}:${this.message}`)
+      logger.error(this.errorForDebug)
+      logger.error("---------------------------")
+    }
+  }
+}
+
+class InvalidInputError extends fileManagerError {
   constructor(message) {
     super(message)
     this.name = "Invalid input"
   }
 }
-class OperationFailedError extends Error {
-  constructor(message) {
-    super(message)
+class OpFieldError extends fileManagerError {
+  constructor(message, errorForDebug = null) {
+    super(message, errorForDebug)
     this.name = "Operation failed"
   }
 }
 
-export { InvalidInputError, OperationFailedError }
+export { InvalidInputError, OpFieldError }
